edde6c87bb794da39eede7d6d21400000334f596
Florian Platz <DO1FPI@darc.de>
2019-04-26 22:12:48 +0200

    Created class PrintHandler.cs in order to give a list of labels and then let this class handle the printing

---
+++ QSLCardPrinter/Helper/PrintHandler.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------
+// <copyright>
+// file = "PrintHandler.cs"
+// project = QSLCardPrinter, QSLCardPrinter
+// last edit 26.04.2019 by Florian Platz (DO1FPI), DO1FPI@darc.de
+// // </copyright>
+// ----------------------------------------------------------------------
+
+namespace QSLCardPrinter.Helper
+{
+    /// <summary>
+    /// Class for handling prints
+    /// </summary>
+    public static class PrintHandler
+    {
+
+        public static void PrintWithPrinterDialog()
+        {
+
+        }
+    }
+}
