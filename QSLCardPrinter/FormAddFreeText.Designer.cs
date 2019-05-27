cd700efb95ed7c9c81a9807d76afc58945ca26dd
Florian Platz <DO1FPI@darc.de>
2019-05-27 20:52:39 +0200

    add form for getting a free text from user

---
+++ QSLCardPrinter/FormAddFreeText.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace QSLCardPrinter
+{
+    partial class FormAddFreeText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFreeText = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxFreeText
+            // 
+            this.textBoxFreeText.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFreeText.Name = "textBoxFreeText";
+            this.textBoxFreeText.Size = new System.Drawing.Size(174, 20);
+            this.textBoxFreeText.TabIndex = 5;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(111, 38);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "&Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.ButtonCancelClick);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 38);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "&Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAddClick);
+            // 
+            // FormAddFreeText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 69);
+            this.Controls.Add(this.textBoxFreeText);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormAddFreeText";
+            this.Text = "FormAddFreeText";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFreeText;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
---
+++ QSLCardPrinter/FormAddFreeText.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------
+// <copyright>
+// file = "FormAddFreeText.cs"
+// project = QSLCardPrinter, QSLCardPrinter
+// last edit 27.05.2019 by Florian Platz (DO1FPI), DO1FPI@darc.de
+// // </copyright>
+// ----------------------------------------------------------------------
+
+namespace QSLCardPrinter
+{
+    #region using directives
+
+    using System;
+    using System.Windows.Forms;
+
+    #endregion
+
+    /// <summary>
+    /// Form for getting the free text
+    /// </summary>
+    public partial class FormAddFreeText : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormAddFreeText"/> class.
+        /// Ask the user for the free text that should be added
+        /// </summary>
+        public FormAddFreeText()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the text returned by user
+        /// </summary>
+        public string ReturnText { get; private set; }
+
+        /// <summary>
+        /// Click on the button add, dialog successful
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="e">event args</param>
+        private void ButtonAddClick(object sender, EventArgs e)
+        {
+            this.ReturnText = this.textBoxFreeText.Text;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        /// <summary>
+        /// Cancel the add, dialog is not successful
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="e">event args</param>
+        private void ButtonCancelClick(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
